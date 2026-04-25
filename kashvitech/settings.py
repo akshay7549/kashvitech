d4fccc32bcb0ac774cd67b4d7b321c39e49b7f08
akshay7549 <akshaykumarstm2020@gmail.com>
2026-04-26 02:15:37 +0530

    fix allowed hosts

--- kashvitech/settings.py
+++ kashvitech/settings.py
@@ -12,7 +12,7 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get(
     'ALLOWED_HOSTS',
-    '.onrender.com,127.0.0.1,localhost'
+    'ALLOWED_HOSTS'
 ).split(',')
 
 
