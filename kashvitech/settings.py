f3c938c6a975a8c1279a805b9cd504bb1a7607db
akshay7549 <akshaykumarstm2020@gmail.com>
2026-04-26 02:34:18 +0530

    fix allowed hosts

--- kashvitech/settings.py
+++ kashvitech/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # ✅ FIXED (syntax + fallback)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['kashvitech-production.up.railway.app']
 
 
 # =========================
